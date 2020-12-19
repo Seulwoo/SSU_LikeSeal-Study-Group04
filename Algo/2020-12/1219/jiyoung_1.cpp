30b20dc4460bc5938aaf3a1f041fb4605947478b
Jiyoung SIM <42690398+Jiyooung@users.noreply.github.com>
2020-12-19 13:32:41 +0900

    Update jiyoung_1.cpp

--- Algo/2020-12/1219/jiyoung_1.cpp
+++ Algo/2020-12/1219/jiyoung_1.cpp
@@ -45,64 +45,69 @@ int main(void) {
         if (find) break;
     }
 
-    for (int i = 0; i < n; i++) {  // 세로
-        for (int j = 0; j < n - 2; j++) {
-            char c = board[j][i];
-            if (c == '.') continue;
-            if (c == board[j + 1][i]) {
-                if (c == board[j + 2][i]) {
-                    answer = c;
-                    find = true;
-                    break;
-                } else {
-                    j = j + 1;
-                    continue;
-                }
-            }
-        }
-        if (find) break;
-    }
-
-    for (int i = 0; i < n - 2; i++) {  // 대각선 오른쪽
-        for (int j = 0; j < n - 2; j++) {
-            if (check[i][j] == 0) {
-                char c = board[i][j];
-                check[i][j] = 1;
+    if (!find) {
+        for (int i = 0; i < n; i++) {  // 세로
+            for (int j = 0; j < n - 2; j++) {
+                char c = board[j][i];
                 if (c == '.') continue;
-                if (c == board[i + 1][j + 1]) {
-                    if (c == board[i + 2][j + 2]) {
+                if (c == board[j + 1][i]) {
+                    if (c == board[j + 2][i]) {
                         answer = c;
                         find = true;
                         break;
                     } else {
-                        check[i + 1][j + 1] = 1;
+                        j = j + 1;
+                        continue;
                     }
                 }
             }
+            if (find) break;
         }
-        if (find) break;
+    }
+    
+    if (!find) {
+        for (int i = 0; i < n - 2; i++) {  // 대각선 오른쪽 ↘
+            for (int j = 0; j < n - 2; j++) {
+                if (check[i][j] == 0) {
+                    char c = board[i][j];
+                    check[i][j] = 1;
+                    if (c == '.') continue;
+                    if (c == board[i + 1][j + 1]) {
+                        if (c == board[i + 2][j + 2]) {
+                            answer = c;
+                            find = true;
+                            break;
+                        } else {
+                            check[i + 1][j + 1] = 1;
+                        }
+                    }
+                }
+            }
+            if (find) break;
+        }
     }
 
-    fill(check[0], check[33], 0);
-
-    for (int i = 0; i < n - 2; i++) {  // 대각선 왼쪽
-        for (int j = 2; j < n; j++) {
-            if (check[i][j] == 0) {
-                char c = board[i][j];
-                check[i][j] = 1;
-                if (c == '.') continue;
-                if (c == board[i + 1][j - 1]) {
-                    if (c == board[i + 2][j - 2]) {
-                        answer = c;
-                        find = true;
-                        break;
-                    } else {
-                        check[i + 1][j - 1] = 1;
+    if (!find) {
+        fill(check[0], check[33], 0);
+        for (int i = 0; i < n - 2; i++) {  // 대각선 왼쪽 ↙
+            for (int j = 2; j < n; j++) {
+                if (check[i][j] == 0) {
+                    char c = board[i][j];
+                    check[i][j] = 1;
+                    if (c == '.') continue;
+                    if (c == board[i + 1][j - 1]) {
+                        if (c == board[i + 2][j - 2]) {
+                            answer = c;
+                            find = true;
+                            break;
+                        } else {
+                            check[i + 1][j - 1] = 1;
+                        }
                     }
                 }
             }
+            if (find) break;
         }
-        if (find) break;
     }
 
     if (find)
